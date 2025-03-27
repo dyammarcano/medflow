5f823e206b658d93c32cfed8ff3edea33ce3b200
Dyam Marcano <dyam.marcano@gmail.com>
2025-03-26 22:35:32 -0300

    Add initial project structure with Go modules and service packages

---
+++ internal/models/models.go
@@ -0,0 +1,29 @@
+package models
+
+const (
+	// SubjectOperationWildcardData operation.*.data
+	SubjectOperationWildcardData = "operation.%s.data"
+
+	// SubjectOperationResponseData operation.response.data
+	SubjectOperationResponseData = "operation.response.data"
+
+	// SubjectOperationIncomingData operation.incoming.data
+	SubjectOperationIncomingData = "operation.incoming.data"
+	// SubjectOperationErrorData operation.error.dada
+	SubjectOperationErrorData = "operation.error.data"
+
+	// SubjectOperationStage1Data operation.stage1.data
+	SubjectOperationStage1Data = "operation.stage1.data"
+
+	// SubjectOperationStage2Data operation.stage2.data
+	SubjectOperationStage2Data = "operation.stage2.data"
+
+	// SubjectOperationStage3Data operation.stage3.data
+	SubjectOperationStage3Data = "operation.stage3.data"
+
+	// SubjectOperationExamsData operation.exams.data
+	SubjectOperationExamsData = "operation.exams.data"
+
+	// SubjectOperationPriority1Data operation.priority1.data
+	SubjectOperationPriority1Data = "operation.priority1.data"
+)
